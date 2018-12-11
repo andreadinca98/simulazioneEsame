d616757a25d5bcfe4a7d4f52f4d7aa449a20e301
Andrea D'Incà <andrea.dinca-1@studenti.unitn.it>
2018-12-11 15:18:58 +0100

    Collegato funzione a chiamata

--- app.js
+++ app.js
@@ -4,13 +4,26 @@ const PORT = process.env.PORT || 3000
  
 app.get('/', (req, res) => res.send('Hello World!'))
  
-app.get('/square/:s', (req, res) => {
-    //console.log("c")
-    var s = req.params.s
-    res.json({result: string_square(s)})
+app.get('/square', (req, res) => {
+    console.log("c")
+    var s = req.query.string
+    square(s)
+    function square(string){
+        console.log('f')
+        var s = string
+        if(s>=0){
+            res.json({result:-1})
+        }
+        else{
+            var l = s.length
+            res.json({result: (l*l)})
+        } 
+    }
+    //var s = req.params.s
+    //res.json({result: string_square(s)})
 })
 
-function string_square(s)
+/*function string_square(s)
 {
     if(s>=0){
         return -1
@@ -19,5 +32,6 @@ function string_square(s)
         var l = s.length
         return(l*l)
     }
-}
-app.listen(PORT, () => console.log('Example app listening on port'+ PORT))
+}*/
+
+app.listen(PORT, () => console.log('Example app listening on port '+ PORT))
