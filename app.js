0de5f5aeac9d863f68a0dddc27bc3a44622ef02f
Andrea D'Incà <andrea.dinca-1@studenti.unitn.it>
2018-12-11 14:58:33 +0100

    Creato funzione

--- app.js
+++ app.js
@@ -2,18 +2,22 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
  
-var courses_offered = [{id: 21, name: 'HCI'},
-{id: 28, name:'sweng'}]
- 
- 
 app.get('/', (req, res) => res.send('Hello World!'))
  
-app.get('/string_square(:s)', (req, res) => {
-   var s = document.getElementById("s").innerHTML
-   string_square(s)
+app.get('/square/:s', (req, res) => {
+    //console.log("c")
+    var s = req.params.s
+    res.json({result: string_square(s)})
 })
+
 function string_square(s)
 {
-    console.log(s)
+    if(s>=0){
+        return -1
+    }
+    else{
+        var l = s.length
+        return(l*l)
+    }
 }
 app.listen(PORT, () => console.log('Example app listening on port'+ PORT))
